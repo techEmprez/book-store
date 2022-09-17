282618c122660aa1734f019eb7cf41f9eec88f6b
techEmprez <choiceosobor@gmail.com>
2022-09-17 15:32:31 +0100

    fix linters error

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import AddBook from '../components/AddBook';
-import Book from '../components/Book';
+import AddBook from '../components/AddBook.js';
+import Book from '../components/Book.js';
 
 function Books() {
   const state = {
